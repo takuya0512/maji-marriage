07b5524b888d32eecdf5bd9b7bc8a4113fd805df
takuya0512 <ta98euleka@gmail.com>
2024-04-04 01:07:17 +0900

    edit更新

--- app/controllers/seatings_controller.rb
+++ app/controllers/seatings_controller.rb
@@ -38,13 +38,11 @@ class SeatingsController < ApplicationController
   end
 
   def update
-    puts 'アプデ前'
     @seating_guest = @seating.guest
 
-    if @seating_guest.update(seating_guest_params)
+    if @seating_guest.update(guest_params)
       redirect_to root_path
     else
-      puts '失敗'
       render :edit, status: :unprocessable_entity
     end
   end
@@ -58,13 +56,17 @@ class SeatingsController < ApplicationController
   private
 
   def set_seating
-    puts 'set_seating実行'
     @seating = Seating.find(params[:id])
   end
 
   def seating_guest_params
     params.require(:seating_guest).permit(:pattern, :name1, :name2, :name3, :name4, :name5, :name6, :name7, :name8, :name9, :name10, :name11, :name12, :name13, :name14, :name15, :name16, :name17, :name18, :name19, :name20, :name21, :name22, :name23, :name24, :name25, :name26, :name27, :name28, :name29, :name30, :name31, :name32, :name33, :name34, :name35, :name36, :name37, :name38, :name39, :name40, :name41, :name42, :name43, :name44, :name45, :name46, :name47, :name48, :name49, :name50, :name51, :name52, :name53, :name54, :name55, :name56, :name57, :name58, :name59, :name60, :name61, :name62, :name63, :name64, :name65, :name66, :name67, :name68, :name69, :name70, :name71, :name72
     ).merge(user_id: current_user.id)
+  end
+
+  def guest_params
+    params.require(:guest).permit(:pattern, :name1, :name2, :name3, :name4, :name5, :name6, :name7, :name8, :name9, :name10, :name11, :name12, :name13, :name14, :name15, :name16, :name17, :name18, :name19, :name20, :name21, :name22, :name23, :name24, :name25, :name26, :name27, :name28, :name29, :name30, :name31, :name32, :name33, :name34, :name35, :name36, :name37, :name38, :name39, :name40, :name41, :name42, :name43, :name44, :name45, :name46, :name47, :name48, :name49, :name50, :name51, :name52, :name53, :name54, :name55, :name56, :name57, :name58, :name59, :name60, :name61, :name62, :name63, :name64, :name65, :name66, :name67, :name68, :name69, :name70, :name71, :name72
+    )
   end
 
   def set_pattern
